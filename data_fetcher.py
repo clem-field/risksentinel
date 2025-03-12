0f601a76c969200299f36a589ba1b7dd089ef40c
clem-field <kc8yhe@me.com>
2025-03-11 20:29:24 -0500

    Added file retrieval, extraction, and clean up

--- data_fetcher.py
+++ data_fetcher.py
@@ -2,22 +2,57 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import urllib.request
+from zipfile import ZipFile as zf
+import os
+import shutil
 
 ##################################################
 ###             Get data from DISA             ###
 ##################################################
+# Get OS Path
+# print(os.getcwd())
 
-disa_url = "https://dl.dod.cyber.mil/wp-content/uploads/stigs/zip/"
-disa_file = "U_SRG-STIG"
+# Set storage locations
+file_location = os.getcwd() + '/file-imports/'
+srg_folder = os.getcwd() + '/srg/'
+stig_folder = os.getcwd() + '/stigs/'
+base_path = os.getcwd()
 
-disa_page = requests.get(disa_url)
+# Set up link
+disa_url = 'https://dl.dod.cyber.mil/wp-content/uploads/stigs/zip/U_SRG-STIG_Library_January_2025.zip'
 
-scrape = bs(disa_page, 'html.parser')
-disa_link = [disa_url + '/' + node.get('fref') for node in scrape.find_all('a') if node.get('href').endswith('zip')]
+# Retrieve File
+get_disa_file = requests.get(disa_url)
 
-for link in disa_link:
-    print(link)
-    filename = link.split('/')[-1]
-    print(filename)
-    # urllib.request.urlretrieve(link,filename)
+# Set file name
+disa_file = disa_url.split('/')[-1]
 
+# Store File
+with open(disa_file,'wb') as output_file:
+    output_file.write(get_disa_file.content)
+print ('STIG Zip retrieved')
+
+# Extract files
+with zf(disa_file, 'r') as zObject:
+    zObject.extractall(
+       path=file_location)
+print(f'Files extracted to: {file_location}')
+
+# Clean up File Download
+if os.path.exists(disa_file):
+    os.remove(disa_file)
+    print(f'Removed {disa_file} from {base_path}')
+else:
+    print('File does not exist')
+
+# Move SRGs
+print(f"Checking for SRG's in: {file_location}")
+for top, dirs, files in os.walk(file_location):
+    for filename in files:
+        if not filename.endswith('SRG.zip'):
+            continue
+        file_path = os.path.join(top, filename)
+        with open(file_path, 'r') as f:
+            if '* Test Outcome : FAIL' in f.read():
+                print(f"Moving file: {filename} to {srg_folder}")
+                shutil.move(file_path, os.path.join(srg_folder, filename))
