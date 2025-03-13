4a3603aede909051a83e078b9004e195f6311081
clem-field <kc8yhe@me.com>
2025-03-13 06:29:57 -0500

    updated modules with parsed functions

--- modules/clean_repo.py
+++ modules/clean_repo.py
@@ -6,7 +6,6 @@
 import os
 from colorama import Fore, Style
 import json
-import sys
 
 def load_config(config_file="config.json"):
     """Load configuration settings from a JSON file.
@@ -66,55 +65,3 @@ def clean_up_files(config, disa_file, base_path):
                     print(Fore.RED + f"Deleting: {dirpath}")
 
 print(Style.RESET_ALL)
-
-
-# ---- Old Code ---
-
-# file_location = os.getcwd() + '/file-imports/'
-# srg_folder = os.getcwd() + '/srgs/'
-# stig_folder = os.getcwd() + '/stigs/'
-# base_path = os.getcwd()
-
-
-
-# # Clean up File Download
-# if os.path.exists(disa_file):
-#     os.remove(disa_file)
-#     print(Fore.RED + f'Removed {disa_file} from {base_path}')
-# else:
-#     print('File does not exist')
-
-# # Clean up Files / Folders in SRG and STIG Directories
-# # Remove non-xml files from SRG directories
-# for subdir, dirs, files in os.walk(srg_folder):
-#     for f in files:
-#         if not (f.endswith('xml')):
-#             print(Fore.RED + f'deleting {f} from {srg_folder}')
-#             file_path = os.path.join(subdir, f)
-#             os.remove(file_path)
-
-# # Remove empty SRG directories
-# for root, dirs, files in os.walk(srg_folder, topdown=False):
-#         for directory in dirs:
-#             dirpath = os.path.join(root, directory)
-#             if not os.listdir(dirpath):
-#                 os.rmdir(dirpath)
-#                 print(Fore.RED + f'Deleting: {dirpath}')
-
-# # Remove non-xml files from STIG directories
-# for subdir, dirs, files in os.walk(stig_folder):
-#     for f in files:
-#         if not (f.endswith('xml')):
-#             print(Fore.RED + f'deleting {f} from {stig_folder}')
-#             file_path = os.path.join(subdir, f)
-#             os.remove(file_path)
-
-# # Remove empty STIG directories
-# for root, dirs, files in os.walk(stig_folder, topdown=False):
-#         for directory in dirs:
-#             dirpath = os.path.join(root, directory)
-#             if not os.listdir(dirpath):
-#                 os.rmdir(dirpath)
-#                 print(Fore.RED + f'Deleting: {dirpath}')
-
-# print(Style.RESET_ALL)
